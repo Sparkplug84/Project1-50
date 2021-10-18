689df2f0c21a2a3546b661920f3e8c359c2ff614
Mark McClean <mcclean_mark_02@hotmail.com>
2021-10-18 17:53:59 +0000

    Project 37 completed

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
-
-for(let i = 0; i < SQUARES; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
-
-    square.addEventListener('mouseover', () => setColor(square))
-    square.addEventListener('mouseout', () => removeColor(square))
-
-    container.appendChild(square)
+const poke_container = document.getElementById('poke-container')
+const pokemon_count = 150
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
 }
 
-function setColor(element) {
-    const color = getRandomColor()
-    element.style.background = color
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+const main_types = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+        }
 }
 
-function removeColor(element) {
-    element.style.background = '#1d1d1d'
-    element.style.boxShadow = '0 0 2px #000'
+getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonCard(data)
 }
 
-function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)]
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+    pokemonEl.style.backgroundColor = color
+
+    const  pokemonInnerHTML = `
+        <div class="img-container">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}">
+        </div>
+        <div class="info">
+            <span class="number">#${id}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+    poke_container.appendChild(pokemonEl)
 }
+
+
+fetchPokemons()
