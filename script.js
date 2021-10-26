0b93bc7fbf97fc653e9deadce44543e3e52d0e62
Mark McClean <mcclean_mark_02@hotmail.com>
2021-10-26 20:43:05 +0000

    Completed until project 44

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const password = document.getElementById('password')
-const background = document.getElementById('background')
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
 
-password.addEventListener('input', (e) => {
-    const input = e.target.value
-    const length = input.length
-
-    const blurValue = 20 - length * 2
-    background.style.filter = `blur(${blurValue}px)`
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
 })
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br />
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feeback to improve our customer support</p>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
